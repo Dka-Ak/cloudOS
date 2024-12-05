5a5cfb921e54e9bac670825d330d40d0a5578460
hitor- 1 <154870205+akishore15@users.noreply.github.com>
2024-12-04 16:19:41 -0800

    run.sh

--- run.sh
+++ run.sh
@@ -1,17 +1,27 @@
 #!/bin/bash
 
+# Check if a secret number is passed as an argument
+if [ -z "$1" ]; then
+    echo "Please provide a secret number."
+    exit 1
+fi
+
+SECRET_NUMBER=$1
+echo "Launching new session with secret number: $SECRET_NUMBER"
+
 # Define the C# file name
 CSHARP_FILE="Program.cs"
 
 # Compile the C# file
-mcs $CSHARP_FILE -out:docker_command_prompt.exe
+mcs $CSHARP_FILE -out:docker_command_prompt_$SECRET_NUMBER.exe
 
 # Check if the compilation was successful
 if [ $? -eq 0 ]; then
     echo "Compilation successful. Running the program..."
 
     # Run the compiled C# program
-    mono docker_command_prompt.exe
+    mono docker_command_prompt_$SECRET_NUMBER.exe
 else
-    echo "Failed. Please correct your code."
+    echo "It has failed. Please correct your code."
 fi
+
